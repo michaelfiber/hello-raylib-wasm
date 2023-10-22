9becba42449e5b5e2eda8347894d2895e4ec2ede
michaelfiber <michael@cubeofb.org>
2023-10-22 14:21:07 -0400

    reorg

--- update-raylib.sh
+++ update-raylib.sh
@@ -3,6 +3,6 @@
 set -Eeuo pipefail
 
 # download the nightly raylib tar.
-pushd ./external > /dev/null
+pushd ./src/external > /dev/null
 wget -qO- https://michaelfiber.github.io/raylib-nightly-wasm/raylib.wasm.tar.gz | tar xvz
 popd > /dev/null
